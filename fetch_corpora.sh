d31317d74953d185225ca2e09b3dcace9093e17a
Jake Lever <jake.lever@gmail.com>
2025-06-30 22:55:00 +0100

    Add semantic network and remove ontologies

--- fetch_corpora.sh
+++ fetch_corpora.sh
@@ -18,6 +18,8 @@ wget -O medmentions/st21pv/corpus_pubtator_pmids_dev.txt https://github.com/chan
 wget -O medmentions/st21pv/corpus_pubtator_pmids_test.txt https://github.com/chanzuckerberg/MedMentions/raw/master/full/data/corpus_pubtator_pmids_test.txt
 wget -O medmentions/st21pv/corpus_pubtator_pmids_trng.txt https://github.com/chanzuckerberg/MedMentions/raw/master/full/data/corpus_pubtator_pmids_trng.txt
 
+wget -O medmentions/SemGroups.txt https://www.nlm.nih.gov/research/umls/knowledge_sources/semantic_network/SemGroups.txt
+
 # GNormPlus
 rm -fr GNormPlusCorpus
 wget -O GNormPlusCorpus.zip https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/tmTools/download/GNormPlus/GNormPlusCorpus.zip
@@ -52,18 +54,3 @@ unzip -d NCBI-disease -o NCBItestset_corpus.zip
 rm NCBItrainset_corpus.zip NCBIdevelopset_corpus.zip NCBItestset_corpus.zip
 
 rm -fr __MACOSX
-
-# MEDIC ontology (from Web Archive which has closest mapping)
-wget -O CTD_diseases.tsv.gz http://web.archive.org/web/20120510030652/http://ctdbase.org/reports/CTD_diseases.tsv.gz
-
-# MeSH 2021 (for NLM-Chem)
-mkdir -p mesh2021
-wget -O mesh2021/desc2021.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2021/xmlmesh/desc2021.xml
-wget -O mesh2021/qual2021.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2021/xmlmesh/qual2021.xml
-wget -O mesh2021/supp2021.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2021/xmlmesh/supp2021.xml
-
-# MeSH 2015 (for BC5CDR)
-mkdir -p mesh2015
-wget -O mesh2015/desc2015.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2015/xmlmesh/desc2015.xml
-wget -O mesh2015/qual2015.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2015/xmlmesh/qual2015.xml
-wget -O mesh2015/supp2015.xml https://nlmpubs.nlm.nih.gov/projects/mesh/2015/xmlmesh/supp2015.xml
